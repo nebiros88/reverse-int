0790bab53db0053dcf700a70b37b13804603a4bb
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2021-03-14 21:42:10 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let strN = n.toString();
+  let result = '';
+  for(let i = strN.length - 1; i >= 0; i--) {
+    if(strN[i] !== '-') {
+        result += strN[i];
+    }  
+  }
+  return +(result);
 }
